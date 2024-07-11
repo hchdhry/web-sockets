81366588dea2b67ba9aa7d558049df0d00be3791
hassan <hchoudhury20@hotmail.com>
2024-07-11 13:30:11 +0100

    create method to send message to all connected users

---
+++ formulaOne.ChatService/hubs/ChatRoom.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace formulaOne.ChatService;
+
+public class ChatRoom : Hub
+{
+    public async Task joinChat(UserConnection userConnection)
+    {
+        await Clients.All.SendAsync(method:"RecievedMessage",arg1:"admin",arg2:$"{userConnection.Username} has joined")
+    }
+
+}
